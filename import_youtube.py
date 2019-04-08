5bce3af14f16f951d0c441fcc9a26445669fa725
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-08 19:25:33 +0300

    Added subs scrapping

--- import_youtube.py
+++ import_youtube.py
@@ -1,6 +1,7 @@
 import pafy
 import os
 import sys
+import subprocess
 
 import constants as const
 
@@ -14,18 +15,42 @@ def download_and_preprocess_data(urls_source_file, out_path):
     audio_out_path = os.path.join(out_path, "audio/")
     if not os.path.isdir(audio_out_path):
         os.makedirs(audio_out_path)
+    
+    subs_out_path = os.path.join(out_path, "subs/")
+    if not os.path.isdir(subs_out_path):
+        os.makedirs(subs_out_path)
+
 
     with open (urls_source_file, "r") as f:
         for url in f:
             try:
                 playlist = pafy.get_playlist(url)
                 for item in playlist['items']:
+                    #downloading subs
+                    filename = os.path.join(subs_out_path, "%(id)s.%(ext)s")
+                    p = subprocess.Popen(["youtube-dl", 
+                        "-o", filename,
+                        "--write-auto-sub",
+                        "--sub-lang", "ru",
+                        "--skip-download",
+                        item['pafy'].videoid],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+                    
+                    out, err = p.communicate()
+
+                    if p.returncode != 0:
+                        raise Exception("Failed to download subs: %s" % str(err))
+                    print("Subs were downloaded")
+
+                    # downloading audio
                     audio = item['pafy'].getbestaudio()
                     filename = os.path.join(audio_out_path, item['pafy'].videoid + "." + audio.extension)
                     result = audio.download(filename)
-                    print(result + " was downloaded")
+                    print(result + " was downloaded")         
+
             except:
-                continue
+                pass
 
 if __name__=="__main__":
     source_file = sys.argv[1]
