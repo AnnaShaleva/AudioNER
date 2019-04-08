9d8ed63aed69a228886332e9111bddbe3ca6bdee
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-08 14:10:30 +0300

    Import data from youtube playlist

--- import_youtube.py
+++ import_youtube.py
@@ -11,12 +11,21 @@ def download_and_preprocess_data(urls_source_file, out_path):
     if not os.path.isdir(out_path):
         os.makedirs(out_path)
 
+    audio_out_path = os.path.join(out_path, "audio/")
+    if not os.path.isdir(audio_out_path):
+        os.makedirs(audio_out_path)
+
     with open (urls_source_file, "r") as f:
         for url in f:
-            p = pafy.new(url)
-            ba = p.getbestaudio()
-            filename = ba.download(out_path)
-            print(filename + " was downloaded")
+            try:
+                playlist = pafy.get_playlist(url)
+                for item in playlist['items']:
+                    audio = item['pafy'].getbestaudio()
+                    filename = os.path.join(audio_out_path, item['pafy'].videoid + "." + audio.extension)
+                    result = audio.download(filename)
+                    print(result + " was downloaded")
+            except:
+                continue
 
 if __name__=="__main__":
     source_file = sys.argv[1]
