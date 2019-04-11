025e1b6b87020b87db1f441db2204a5bd911b852
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-11 13:22:02 +0300

    Indexing fixes: Dataset --> subs --> doc_with_filename

--- vtt_indexer.py
+++ vtt_indexer.py
@@ -22,39 +22,45 @@ def parse_subs_into_line_tockens_list(source_path):
             result.append(line)
     return result
 
-def parse_subs_into_word_tockens_list(source_path):
+def parse_subs_into_word_tockens_list(source_path, subs_file):
     result = []
     for caption in webvtt.read(source_path):
-        line = caption.raw_text.split('\n')[1]
-        line = re.sub('[<][\/]?[c][^<]*[>]', "", line)
-        line = line.replace(" ", "").lower()
-        if line:
-            line = "<" + caption.start + ">" + line + "<" + caption.end + ">"
-            tockens = list(filter(None, re.split('[<>]', line)))
-            count = 0
-            while count < len(tockens) - 2:
-                start = tockens[count]
-                text = tockens[count + 1]
-                end = tockens[count + 2]
-                toAppend = {
-                        'start': start,
-                        'end': end,
-                        'text': text
-                        }
-                result.append(toAppend)
-                count += 2
+        arr = caption.raw_text.split('\n')
+        if(len(arr) > 1):
+            line = arr[1]
+            line = re.sub('[<][\/]?[c][^<]*[>]', "", line)
+            line = line.replace(" ", "").lower()
+            if line:
+                line = "<" + caption.start + ">" + line + "<" + caption.end + ">"
+                tockens = list(filter(None, re.split('[<>]', line)))
+                count = 0
+                while count < len(tockens) - 2:
+                    start = tockens[count]
+                    text = tockens[count + 1]
+                    end = tockens[count + 2]
+                    toAppend = {
+                            'filename': subs_file,
+                            'start': start,
+                            'end': end,
+                            'text': text
+                            }
+                    result.append(toAppend)
+                    count += 2
     return result
 
-def index_subs_dataset(dataset_path):
-    es = Elasticsearch()
+def index_subs_dataset(dataset_name):
+    dataset_path = os.path.join(const.DATA_DIR, dataset_name + "/", "subs/")
+    es = Elasticsearch(
+            [const.ELASTIC_HOST]
+            )
+    num = 1
     for subs_file in os.listdir(dataset_path):
-        num = 1
-        for element in parse_subs_into_word_tockens_list(os.path.join(dataset_path, subs_file)):
-            es.index(index="subs_index", doc_type=subs_file, id=num, body=element)
+        for element in parse_subs_into_word_tockens_list(os.path.join(dataset_path, subs_file), subs_file):
+            es.index(index=dataset_name, doc_type="subs", id=num, body=element)
             num += 1
+            print(element)
 
 if __name__=="__main__":
-    dataset_folder = sys.argv[1]
-    dataset_path = os.path.join(const.DATA_DIR, dataset_folder, "subs/")
-#out_path = os.path.join(const.DATA_DIR, sys.argv[2])
-    index_subs_dataset(dataset_path)
+    dataset_name = sys.argv[1]
+    #out_path = os.path.join(const.DATA_DIR, sys.argv[2])
+    index_subs_dataset(dataset_name)
