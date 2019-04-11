7c4beb07995ccc4926effcd2b3bbf6bbf6902763
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-11 13:22:40 +0300

    Added elastic parametrs to constants

--- constants.py
+++ constants.py
@@ -7,3 +7,8 @@ PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 CATEGORIES_DIR = os.path.join(PROJECT_ROOT_PATH, "categories/")
 DATA_DIR = os.path.join(PROJECT_ROOT_PATH, "data/")
 
+
+### PARAMETRS ###
+
+# ElasticSearch Settings
+ELASTIC_HOST = "localhost:9200"
