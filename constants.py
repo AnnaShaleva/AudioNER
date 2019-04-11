805cccaa899c750bcd69bc3e5c3ff562bce5ac61
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-11 15:01:12 +0300

    Fixed paths

--- constants.py
+++ constants.py
@@ -4,8 +4,8 @@ import os
 
 # Main Paths
 PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
-CATEGORIES_DIR = os.path.join(PROJECT_ROOT_PATH, "categories/")
-DATA_DIR = os.path.join(PROJECT_ROOT_PATH, "data/")
+CATEGORIES_PATH = os.path.join(PROJECT_ROOT_PATH, "categories/")
+DATA_PATH = os.path.join(PROJECT_ROOT_PATH, "data/")
 
 
 ### PARAMETRS ###
