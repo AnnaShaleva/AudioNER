03866663f625e774e85d19da333d2f1bd4cf8b0f
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-15 21:20:54 +0300

    Added Name category

--- categories.py
+++ categories.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 import constants as const
 
+
 def fill_number_category(out_path):
     numbers = [
             "ноль",
@@ -23,6 +24,7 @@ def fill_number_category(out_path):
         f.write(i + "\r\n")
     f.close()
 
+
 def fill_direction_category(out_path):
     directions = [
             "вперед",
@@ -34,22 +36,31 @@ def fill_direction_category(out_path):
             "восток",
             "запад"
             ]
-    f = open(out_path, "w")
-    for d in directions:
-        f.write(d + "\r\n")
-    f.close()
+    with open(out_path, "w") as f:
+        for d in directions:
+            f.write(d + "\r\n")
+
+
+def fill_name_category(out_path):
+    names_frame = pd.read_csv(os.path.join(const.DATA_PATH, 'russian_names.csv'), header=0, sep=';')
+    with open(out_path, 'w') as f:
+        frame = names_frame.sort_values('PeoplesCount', ascending=False)
+        for row in frame.head(n=300).itertuples():
+            f.write(row.Name.lower().replace('ё', 'е') + '\n')
+
 
 def fill_categories():
-    if not os.path.exists(const.CATEGORIES_DIR):
-        os.makedirs(const.CATEGORIES_DIR)
+    if not os.path.exists(const.CATEGORIES_PATH):
+        os.makedirs(const.CATEGORIES_PATH)
         
-    categories = ["number", "direction"]
+    categories = ["number", "direction", "name", "surname"]
     for category in categories:
         filename = category + ".txt"
         out_path = os.path.join(const.CATEGORIES_DIR, filename)
 
         function_name = "fill_" + category + "_category"
         globals()[function_name](out_path)
+
 
 if __name__=="__main__":
     fill_categories()
