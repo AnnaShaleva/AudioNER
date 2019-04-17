14a073f6e31744d8d1be45f330013edcfb4e874e
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-17 13:19:45 +0300

    Fixes

--- categories.py
+++ categories.py
@@ -43,7 +43,7 @@ def fill_direction_category(out_path):
 
 def fill_name_category(out_path):
     names_frame = pd.read_csv(os.path.join(const.DATA_PATH, 'russian_names.csv'), header=0, sep=';')
-    with open(out_path, 'w') as f:
+    with open(out_path, 'w', encoding='utf8') as f:
         frame = names_frame.sort_values('PeoplesCount', ascending=False)
         for row in frame.head(n=300).itertuples():
             f.write(row.Name.lower().replace('ё', 'е') + '\n')
@@ -53,10 +53,10 @@ def fill_categories():
     if not os.path.exists(const.CATEGORIES_PATH):
         os.makedirs(const.CATEGORIES_PATH)
         
-    categories = ["number", "direction", "name", "surname"]
+    categories = ["number", "direction", "name"]
     for category in categories:
         filename = category + ".txt"
-        out_path = os.path.join(const.CATEGORIES_DIR, filename)
+        out_path = os.path.join(const.CATEGORIES_PATH, filename)
 
         function_name = "fill_" + category + "_category"
         globals()[function_name](out_path)
