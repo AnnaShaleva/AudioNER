aba097d61c10ecb1a2b0ee22d2e30c9014f41431
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-28 08:29:34 +0000

    Util for removing extra subs or audio

--- util.py
+++ util.py
@@ -15,22 +15,26 @@ def match_subs_audio(dataset_name):
         if not os.path.isfile(audio_file):
             print('Remove:' + sub_file)
             count +=1
-            #os.remove(os.path.join(subs_path, sub_file))
+            os.remove(os.path.join(subs_path, sub_file))
+    print(str(count))
+
 
 def match_audio_subs(dataset_name):
     print('######## MATCH AUDIO SUBS #############')
     subs_path = (os.path.join(const.DATA_PATH, dataset_name + '/', 'subs/'))
     audio_path = (os.path.join(const.DATA_PATH, dataset_name + '/', 'audio/'))
-
+    count = 0
     for audio_file in os.listdir(audio_path):
         video_id = os.path.splitext(audio_file)[0]
         #print(video_id)
-        count = 0
+       
         sub_file = os.path.join(subs_path, video_id + '.ru.vtt')
         if not os.path.isfile(sub_file):
             print('Remove:' + audio_file)
             count+=1
-            #os.remove(os.path.join(audio_path, audio_file))
+            os.remove(os.path.join(audio_path, audio_file))
+
+    print(str(count))
 
 if __name__ == "__main__":
     dataset_name = sys.argv[1]
