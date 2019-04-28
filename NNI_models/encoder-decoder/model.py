4cb99df46a4ec373e32d0f50b48468596df999f6
AnnaShaleva <shaleva.ann@gmail.com>
2019-04-28 08:11:00 +0000

    Added 3 models

--- NNI_models/encoder-decoder/model.py
+++ NNI_models/encoder-decoder/model.py
@@ -7,18 +7,22 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.python.keras.callbacks import TensorBoard
-from keras.utils import to_categorical, plot_model
-from keras.models import Model
-from keras.layers import Dense, Flatten, Activation, Dropout, Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Flatten, Activation, Dropout, Conv2D, MaxPooling2D, UpSampling2D
 import nni
-
-import constants as const
+sys.path.insert(0, '/headless/shared/AudioNER/')
+import constants as const 
 from load_data import get_test_and_train_data
 
 
+#os.environ['NNI_OUTPUT_DIR'] = '/headless/shared/nni_logs/'
 LOG = logging.getLogger('encoder-decoder_model_keras')
 TENSORBOARD_DIR = os.environ['NNI_OUTPUT_DIR']
 
+
+def mean_squared_error(val1, val2):
+    return (val1 * val1 + val2 * val2) ** 0.5
 
 def load_dataset(dataset_name, part_train):
     print('Load data...')
@@ -37,11 +41,13 @@ def load_dataset(dataset_name, part_train):
 def build_model(hyper_params, input_shape=(const.HEIGHT, const.LENGTH, 1), num_classes=const.N_CLASSES, num_subclasses=const.N_SUBCLASSES):
     print('Building model...')
     #input layer
-    visible = Conv2D(16, kernel_size=hyper_params['conv_size'], padding='same', activation='relu', input_shape=input_shape)
+
+    visible = Input(shape=input_shape)
 
     #classification layers
     #encoder
-    pool1 = MaxPooling2D((2, 2), padding='same')(visible)
+    conv0 = Conv2D(16, kernel_size=hyper_params['conv_size'], padding = 'same', activation = 'relu')(visible)
+    pool1 = MaxPooling2D((2, 2), padding='same')(conv0)
     conv1 = Conv2D(32, kernel_size=hyper_params['conv_size'], padding = 'same', activation = 'relu')(pool1)
     pool2 = MaxPooling2D((2, 2), padding='same')(conv1)
     conv2 = Conv2D(64, kernel_size=hyper_params['conv_size'], padding = 'same', activation = 'relu')(pool2)
@@ -67,20 +73,20 @@ def build_model(hyper_params, input_shape=(const.HEIGHT, const.LENGTH, 1), num_c
     dense3 = Dense(hyper_params['hidden_size'] / 4)(dense2)
     subclasses_output = Dense(num_subclasses, activation = 'softmax')(dense3)
 
-    model = Model(input=visible, outputs=[classes_output, subclasses_output])
+    model = Model(inputs=visible, outputs=[classes_output, subclasses_output])
 
     # summarize layers
     print(model.summary())
 
     # plot graph
-    plot_model(model, to_file='multiple_outputs.png')
+#    plot_model(model, to_file='multiple_outputs.png')
 
     if hyper_params['optimizer'] == 'Adam':
         optimizer = keras.optimizers.Adam(lr=hyper_params['learning_rate'])
     else:
         optimizer = keras.optimizers.SGD(lr=hyper_params['learning_rate'], momentum=0.9)
 
-    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=optimizer, metrics=['accuracy'])
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=optimizer, metrics=[keras.metrics.categorical_accuracy])
     return model
 
 
@@ -93,6 +99,7 @@ class SendMetrics(keras.callbacks.Callback):
         Run on end of each epoch
         '''
         LOG.debug(logs)
+        print(logs)
         nni.report_intermediate_result(logs["val_acc"])
 
 
@@ -101,10 +108,10 @@ def train(args, params):
     Train model
     '''
     model = build_model(params)
-    X_train, Y1_train, Y2_train, X_test, Y1_test, Y2_test = load_dataset(args.dataset_name, args.num_train)
+    X_train, Y1_train, Y2_train, X_test, Y1_test, Y2_test = load_dataset(args.dataset_name, float(args.num_train))
 
     print('Fitting model...')
-    results = model.fit(X_train, [Y1_train, Y2_train], batch_size=args.batch_size, epochs=args.epochs, verbose=1,
+    results = model.fit(X_train, [Y1_train, Y2_train], epochs=args.epochs, verbose=1,
                         validation_data=(X_test, [Y1_test, Y2_test]), callbacks=[SendMetrics(), TensorBoard(log_dir=TENSORBOARD_DIR)])
 
     _, acc = model.evaluate(X_test, [Y1_test, Y2_test], verbose=0)
@@ -124,8 +131,8 @@ def generate_default_params():
 
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser()
-    PARSER.add_argument("--dataset_name", type=str, help='Dataset name', required=True)
-    PARSER.add_argument("--epochs", type=int, default=1000, help="Train epochs", required=False)
+    PARSER.add_argument("--dataset_name", type=str, default='encoder_decoder_dataset', help='Dataset name', required=False)
+    PARSER.add_argument("--epochs", type=int, default=100, help="Train epochs", required=False)
     PARSER.add_argument("--num_train", type=float, default=0.8,
                         help="Part of train samples to be used, maximum 1", required=False)
 
