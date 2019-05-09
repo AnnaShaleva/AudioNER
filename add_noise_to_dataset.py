2a1e12e34b0406cafd6bbe7a555ea8dbb15f7e0b
AnnaShaleva <shaleva.ann@gmail.com>
2019-05-09 17:27:46 +0300

    Add large noise

--- add_noise_to_dataset.py
+++ add_noise_to_dataset.py
@@ -10,12 +10,18 @@ def add_noise_to_audio(path, name):
     sr_value, x_value = scipy.io.wavfile.read(source_sample_path)
     target_snr = 100000
     sig_avg_x = np.mean(x_value)
-    noise_avg_x = abs(sig_avg_x * target_snr)
-    noise = np.random.normal(.0, np.sqrt(noise_avg_x), len(x_value)).round().astype(int)
-    y_value = np.asarray(x_value + noise, dtype=np.int16)
+    noise_avg_small = abs(sig_avg_x * target_snr)
+    noise_avg_large = abs(sig_avg_x * target_snr)
+    noise_small = np.random.normal(.0, np.sqrt(noise_avg_small), len(x_value)).round().astype(int)
+    noise_large = np.random.normal(.0, np.sqrt(noise_avg_large), len(x_value)).round().astype(int)
+    y_value = np.asarray(x_value + noise_small, dtype=np.int16)
+    z_value = np.asarray(x_value + noise_large, dtype=np.int16)
 
-    dest_sample_path = path + os.path.splitext(name)[0] + '_noised.wav'
+    dest_sample_path = path + os.path.splitext(name)[0] + '_small_noised.wav'
     scipy.io.wavfile.write(dest_sample_path, sr_value, y_value)
+
+    dest_sample_path = path + os.path.splitext(name)[0] + '_large_noised.wav'
+    scipy.io.wavfile.write(dest_sample_path, sr_value, z_value)
 
 if __name__=='__main__':
     dataset_name = sys.argv[1]
