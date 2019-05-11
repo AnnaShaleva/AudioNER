ac9ccb05794fb442083eb4affb0cf5f1ac6aa5e1
AnnaShaleva <shaleva.ann@gmail.com>
2019-05-11 20:47:29 +0300

    one level of noise

--- add_noise_to_dataset.py
+++ add_noise_to_dataset.py
@@ -46,18 +46,18 @@ def add_noise_to_audio(path, name):
     source_sample_path = path + name
     
     sr_value, x_value = scipy.io.wavfile.read(source_sample_path)
-    target_snr_low = 2
+    #target_snr_low = 2
     target_snr_large = 10
     sig_avg_x = np.mean(abs(x_value))
-    noise_avg_small = sig_avg_x * target_snr_low
+    #noise_avg_small = sig_avg_x * target_snr_low
     noise_avg_large = sig_avg_x * target_snr_large
-    noise_small = np.random.normal(.0, np.sqrt(noise_avg_small), len(x_value)).round().astype(int)
+    #noise_small = np.random.normal(.0, np.sqrt(noise_avg_small), len(x_value)).round().astype(int)
     noise_large = np.random.normal(.0, np.sqrt(noise_avg_large), len(x_value)).round().astype(int)
-    y_value = np.asarray(x_value + noise_small, dtype=np.int16)
+    #y_value = np.asarray(x_value + noise_small, dtype=np.int16)
     z_value = np.asarray(x_value + noise_large, dtype=np.int16)
 
-    dest_sample_path = path + os.path.splitext(name)[0] + '_small_noised.wav'
-    scipy.io.wavfile.write(dest_sample_path, sr_value, y_value)
+    #dest_sample_path = path + os.path.splitext(name)[0] + '_small_noised.wav'
+    #scipy.io.wavfile.write(dest_sample_path, sr_value, y_value)
 
     dest_sample_path = path + os.path.splitext(name)[0] + '_large_noised.wav'
     scipy.io.wavfile.write(dest_sample_path, sr_value, z_value)
@@ -71,8 +71,8 @@ if __name__=='__main__':
         category_source_path = dataset_path + category_folder + '/'
         for subcat_folder in os.listdir(category_source_path):
             subcat_source_path = category_source_path + subcat_folder + '/'
-            #for sample in os.listdir(subcat_source_path):
-                #speed_up_audio(subcat_source_path, sample)
+            for sample in os.listdir(subcat_source_path):
+                speed_up_audio(subcat_source_path, sample)
             for sample in os.listdir(subcat_source_path):
                 add_noise_to_audio(subcat_source_path, sample)
 
