c8c5b8e162d199d401eff85d8fed3adb43248a87
AnnaShaleva <shaleva.ann@gmail.com>
2019-05-27 11:25:41 +0300

    Fix

--- interpreter.py
+++ interpreter.py
@@ -6,22 +6,24 @@ style.use('fivethirtyeight')
 
 fig = plt.figure()
 main_plot = fig.add_subplot(1,1,1)
+plt.xlabel('x')
+plt.ylabel('y')
+
 
 def animate(i):
     with open('commands.txt', 'r') as data:
         lines = data.read().split('\n')
-        X = []
-        Y = []
-        X.append(0)
-        Y.append(1)
+        X = [0]
+        Y = [0]
         for line in lines:
             if(len(line) > 1):
                 delta_x, delta_y = line.split(',')
-                X.append(X[-1] + delta_x)
-                Y.append(Y[-1] + delta_y)
+                X.append(X[-1] + int(delta_x))
+                Y.append(Y[-1] + int(delta_y))
         main_plot.clear()
         main_plot.plot(X, Y)
 
 
-animation.FuncAnimation(fig, animate, interval=1000)
-plt.show()
+if __name__ == '__main__':
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    plt.show()
